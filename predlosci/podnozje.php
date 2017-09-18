17c208348f1b928f349eb867f2100526ab2152fe
Hrvoje <hrvojesen@gmail.com>
2017-09-18 15:21:45 +0200

    Add files via upload

--- predlosci/podnozje.php
+++ predlosci/podnozje.php
@@ -1,11 +1,11 @@
 <div class="row" align="center">
 	
-	&copy; <?php echo $naslovAplikacije . ","; date("Y"); ?> 
+	&copy; <?php echo $naslovAplikacije; date("Y"); ?> 
 	
 	<?php 
 	
 	if($_SERVER["HTTP_HOST"]==="localhost"){
-		echo "<span style=\"color: red\">Lokalno</span>";
+		echo "- <span style=\"color: red\">Lokalno</span>";
 	}
 	
 	?>
